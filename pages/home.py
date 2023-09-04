7672c28d43986473766c91c192601fe29c8f62c1
Russell Romney <russdude7@gmail.com>
2023-09-04 17:27:59 -0600

    add some more body

--- pages/home.py
+++ pages/home.py
@@ -10,5 +10,30 @@ def layout():
         [
             html.H1("Welcome to the home page."),
             html.P("This page is accessible to anyone."),
+            html.P(
+                "This is a Dash Pages app that uses Flask-Login in a multi-page app structure."
+            ),
+            html.P(
+                [
+                    "Check out the code at ",
+                    html.A(
+                        "Github: dash-flask-login",
+                        href="https://github.com/russellromney/dash-flask-login",
+                        target="_blank",
+                        referrerPolicy="no-referrer-when-downgrade",
+                    ),
+                ]
+            ),
+            html.P(
+                [
+                    "Check out an example of an advanced Dash authentication flow at ",
+                    html.A(
+                        "Github: dash-auth-flow",
+                        href="https://github.com/russellromney/dash-auth-flow",
+                        target="_blank",
+                        referrerPolicy="no-referrer-when-downgrade",
+                    ),
+                ]
+            ),
         ]
     )
